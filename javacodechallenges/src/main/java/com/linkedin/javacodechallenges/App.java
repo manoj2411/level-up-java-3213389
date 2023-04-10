d6fd2aaeef0906103573c2bfbadcce08a2f53e68
Manoj Sehrawat <sehrawat.manoj24@gmail.com>
2023-04-10 09:47:08 +0000

    implement calculatewaterbill

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -3,10 +3,23 @@ package com.linkedin.javacodechallenges;
 import java.util.Scanner;
 
 public class App {
+    private static final double MIN_CHARGE = 18.84;
+    private static final int ONE_CCF = 748;
+    private static final double CHARGE = 3.9;
+    private static final int BASE_CCF = 2;
 
     public static double calculateWaterBill(double gallonsUsage) {
-        // TODO: Implement method
-        return 0;
+        double extraUsage = gallonsUsage - (ONE_CCF * BASE_CCF);
+        int extraUnits = getExtraUnits(extraUsage);
+
+        return MIN_CHARGE + (extraUnits * CHARGE);
+    }
+
+    private static int getExtraUnits(double gallonsUsage) {
+        if (gallonsUsage <= 0) {
+            return 0;
+        }
+        return (int)Math.ceil(gallonsUsage / ONE_CCF);
     }
 
     public static void main(String[] args) {
